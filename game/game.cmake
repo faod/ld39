21c3a12008b0be91dd94c4ad5ff0a2775142a0be
baylej <baylej@mrhide.eu>
2017-07-29 14:18:47 +0200

    Add tmx 1.0.0

--- game/game.cmake
+++ game/game.cmake
@@ -2,4 +2,22 @@
 add_sources(
   src/game.cpp
   src/cyclist.cpp
+  src/tmx/tmx.c
+  src/tmx/tmx_err.c
+  src/tmx/tmx_hash.c
+  src/tmx/tmx_mem.c
+  src/tmx/tmx_utils.c
+  src/tmx/tmx_xml.c
+  src/tmx/tsx.c
 )
+
+include(FindLibXml2)
+find_package(LibXml2 REQUIRED)
+list(APPEND Vivace_Include_Dirs ${LIBXML2_INCLUDE_DIR})
+list(APPEND Vivace_Libraries ${LIBXML2_LIBRARIES})
+
+include(FindZLIB)
+find_package(ZLIB REQUIRED)
+list(APPEND Vivace_Include_Dirs ${ZLIB_INCLUDE_DIR})
+list(APPEND Vivace_Libraries ${ZLIB_LIBRARIES})
+list(APPEND Vivace_Definitions "WANT_ZLIB")
